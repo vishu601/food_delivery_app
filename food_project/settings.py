f31154d5bb22edc8e01b04cff81c2852afec97f1
vishu601 <vishu601@gmail.com>
2026-07-08 06:12:37 +0000

    Render deployment ke liye settings update ki

--- food_project/settings.py
+++ food_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jw91(7kfhg=do_zekj0*8%x=e*wzej0emhj*0od351h8!cr1^1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
